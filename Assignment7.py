adc1557073f93cf310abd101bcb7ff9d994d34c8
Dhvani <dkm12305@gmail.com>
2023-07-02 14:33:52 +0530

    commit

--- Assignment7.py
+++ Assignment7.py
@@ -1,16 +1,14 @@
-# 1) Create a function with a default parameter "file" storing the file path
-# 2) Open the "file" in append mode
-# 3) Use writelines() method to add your roll number, name, and class
-# 4) Use readines() method to print your data in the prompt
-
-# Note: Use try...except block with suitable exception class
 def file_operation(roll_no, name,stud_class, file="data.txt"):
     try:
+        f=open(file, "w")
+        pass
         f=open(file,"a")
-        f.write(f"{roll_no} , {name} and {stud_class} ")
+        line=[f"{roll_no}, {name}, {stud_class}"]
+        f.writelines(line)
         f=open(file,"r")
-        data=f.read()
+        data=f.readlines()
         print("Data is: ")
+
         print(data)
 
     except FileNotFoundError:
